267b22c2fb87a0bdfb9895844a139c4766730357
sindris12 <sindris12@ru.is>
2014-12-02 14:18:55 +0000

    Create Game Test finished

--- server/model/tictactoe/gameInstanceCreated.spec.js
+++ server/model/tictactoe/gameInstanceCreated.spec.js
@@ -6,3 +6,34 @@ var should = require('should');
 var _ = require('lodash');
 var tictactoe = require('./tictactoe');
 
+describe('Create Game', function() {
+
+  it('should create a instance of the game', function() {
+    var given = [];
+
+    var when = {
+      command: "CreateGame",
+      user: {
+        userName: "Sindri"
+      },
+      name: "EliteTicTacToe",
+      timeStamp: "2014-12-02T11:29:29"
+    }
+
+    var then = [{
+      event: "GameCreated",
+      user: {
+        userName: "Sindri"
+      },
+      name: "EliteTicTacToe",
+      timeStamp: "2014-12-02T11:29:29"
+    }];
+
+    var actualEvent = tictactoe(given).execudeCommand(when);
+
+    should(actualEvent.length).be.exactly(1);
+
+    should(JSON.stringify(actualEvent)).be.exactly(JSON.stringify(then));
+  })
+});
+
--- server/model/tictactoe/tictactoe.js
+++ server/model/tictactoe/tictactoe.js
@@ -2,6 +2,43 @@
  * Created by sindrisigurjonsson on 02/12/14.
  */
 
-module.exports = function(history){
-  return null;
-}
+module.exports = function(history) {
+
+  var tictactoeState = require('./tictactoeState');
+  var gameState = tictactoeState(history);
+
+  return {
+    execudeCommand: function (command) {
+      var commandHandler = {
+        "CreateGame": function (command) {
+          return [{
+            event: "GameCreated",
+            user: command.user,
+            name: command.name,
+            timeStamp: command.timeStamp
+          }]
+        },
+        "JoinGame": function (command) {
+          if (gameState.gameFull()) {
+            console.log("Sorry the game is full!");
+            return [{
+              event: "FullGameJoinAttempted",
+              user: command.user,
+              name: command.name,
+              timeStamp: command.timeStamp
+            }];
+          }
+          console.log("Game joined");
+          gameFull = true;
+          return [{
+            event: "GameJoined",
+            user: command.user,
+            name: command.name,
+            timeStamp: command.timeStamp
+          }];
+        }
+      }
+      return commandHandler[command.command](command);
+    }
+  }
+};
