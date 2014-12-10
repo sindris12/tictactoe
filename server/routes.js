1447a559b92381781a4295f3bd108725fb8b788f
sindris12 <sindris12@ru.is>
2014-12-10 16:40:48 +0000

    removed some files

--- server/routes.js
+++ server/routes.js
@@ -9,7 +9,6 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/createGame', require('./api/createGame')(app).router);
   app.use('/api/joinGame', require('./api/joinGame')(app).router);
   app.use('/api/placeMove', require('./api/placeMove')(app).router);
