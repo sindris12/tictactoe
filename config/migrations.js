755fca716256c8a8f35e2c15f4846fff02397ca4
sindris12 <sindris12@ru.is>
2014-12-16 13:22:47 +0000

    mongo name was wrong !

--- config/migrations.js
+++ config/migrations.js
@@ -12,7 +12,7 @@ module.exports = {
   acceptance: {
     schema: { 'migration': {} },
     modelName: 'Migration',
-    db: process.env.MONGOHQ_URL || 'mongodb://admin:admin@ds063630.mongolab.com:63630/tictactoe-acceptance'
+    db: process.env.MONGOHQ_URL || 'mongodb://admin:admin@ds063630.mongolab.com:63630/tictactoe-test'
   }/*,
   production: {
     schema: { 'migration': {} },
