6b9b1c6adc0a76e9973e110d51cbf83d83363683
sindris12 <sindris12@ru.is>
2014-12-16 18:41:16 +0000

    this is killing mee !

--- ts.sh
+++ ts.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 set -e
+echo "Killing docker image"
 docker kill $(docker ps -a -q)
+echo "Pulling image"
 docker pull sindris12/tictactoe
+echo "Run mongoose-migrate"
 docker run -e "NODE_ENV=acceptance" sindris12/tictactoe ./node_modules/.bin/mongoose-migrate
+echo "set up acceptance"
 docker run -p 80:8080 -d -e "NODE_ENV=acceptance" sindris12/tictactoe
