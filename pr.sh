0c65d8effc8afc69213b52aaf3fef9e21ec74afb
sindris12 <sindris12@ru.is>
2014-12-17 14:54:17 +0000

    whops ! wrong env

--- pr.sh
+++ pr.sh
@@ -5,6 +5,6 @@ docker kill $(docker ps -a -q)
 echo "Pulling"
 docker pull sindris12/tictactoe
 echo "Run mongoose-migrate"
-docker run -e "NODE_ENV=acceptance" sindris12/tictactoe ./node_modules/.bin/mongoose-migrate
+docker run -e "NODE_ENV=production" sindris12/tictactoe ./node_modules/.bin/mongoose-migrate
 echo "set up acceptance"
 docker run -p 80:8080 -d -e "NODE_ENV=production" sindris12/tictactoe
