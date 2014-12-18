1b1b5d9d9321b1348c7de53925482047678654f2
sindris12 <sindris12@ru.is>
2014-12-18 10:47:53 +0000

    adding url to the production server

--- pr.sh
+++ pr.sh
@@ -6,5 +6,5 @@ echo "Pulling"
 docker pull sindris12/tictactoe
 echo "Run mongoose-migrate"
 docker run -e "NODE_ENV=production" sindris12/tictactoe ./node_modules/.bin/mongoose-migrate
-echo "set up acceptance"
+echo "set up production"
 docker run -p 80:8080 -d -e "NODE_ENV=production" sindris12/tictactoe
